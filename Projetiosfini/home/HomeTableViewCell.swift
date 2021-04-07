422be4c51a27f9b8b929ae5a393fc969387e6ca4
Fyreti <ygorraeyahoo.fr>
2021-04-07 10:06:44 +0100

    Projet fini sans les test

--- Projetiosfini/home/HomeTableViewCell.swift
+++ Projetiosfini/home/HomeTableViewCell.swift
@@ -13,14 +13,12 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var Event: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
         
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
--- Projetiosfini/sponsors/SponsorsTableViewCell.swift
+++ Projetiosfini/sponsors/SponsorsTableViewCell.swift
@@ -16,13 +16,11 @@ class SponsorsTableViewCell: UITableViewCell {
     @IBOutlet weak var sponsorName: UILabel?
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
